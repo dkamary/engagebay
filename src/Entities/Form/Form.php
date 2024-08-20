b5e725d25c31b953aaf3f70cea0179c999ed6c4f
Donatien Kamary <donatkamary@gmail.com>
2024-08-20 18:39:54 +0000

    collection compatibility with 7.3 and form properties

--- src/Entities/Form/Form.php
+++ src/Entities/Form/Form.php
@@ -4,5 +4,28 @@ namespace Aika\Engagebay\Form;
 
 class Form
 {
-    // 
+    public $id;
+    public $name;
+    public $alias_name;
+    public $owner_id;
+    public $created_time;
+    public $updated_time;
+    public $formHtml;
+    public $form_attributes;
+    public $enable_whitelabel;
+    public $form_style;
+    public $version;
+    public $incentiveEmail;
+    public $thumbnail;
+    public $formStats;
+
+    public function __construct(array $data = [])
+    {
+        $this->hydrate($data);
+    }
+
+    public function hydrate(array $dat): static
+    {
+        //
+    }
 }
